c221d43b0c1d925a93a9d1dbca2de92daf3d53fa
Florian Garcia <florian.garcia@outlook.fr>
2017-11-24 14:28:49 +0100

    wrote index.php router for MVC

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Index du site
+ */
+
+// Initialisation des paramètres du site
+require_once('./config/configuration.php');
+require_once(PATH_TEXTES.LANG.'.php');
+
+//vérification de la page demandée 
+
+if(isset($_GET['page']) && is_file(PATH_CONTROLLERS.$_GET['page'].".php")) 
+	$page = $_GET['page']; // http://.../index.php?page=toto
+elseif(!isset($_GET['page']))
+	$page="index"; //page d'accueil du site - http://.../index.php
+else 
+	$page="404"; //page demandée inexistante
+
+//appel du controller
+require_once(PATH_CONTROLLERS.$page.'.php'); 
+
+?>
