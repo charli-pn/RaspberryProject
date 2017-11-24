e4a92317dffe6dd825d0a78237d634a0264e7ebd
Unknown <charlottepineau98@gmail.com>
2017-11-24 15:17:36 +0100

    start display music

--- views/v_header.php
+++ views/v_header.php
@@ -13,6 +13,8 @@
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
         <script type="text/javascript" src="js/materialize.min.js"></script>
         <h1>Let's play some music !</h1>
+        <?php require_once("v_menu.php"); ?>
+        <?php require_once("v_play.php"); ?>
     
     
     
