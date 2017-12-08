4a38696553c96798f4583ee4b2d363e462d483c1
Cha ! <charlottepineau98@gmail.com>
2017-12-08 15:21:24 +0100

    Modification form

--- views/v_add.php
+++ views/v_add.php
@@ -7,27 +7,38 @@ require_once(PATH_VIEWS."header.php");?>
 <div class="divider"></div>
 
 <div class="row">
-<div class="col s6">
-<form method="post" enctype="multipart/form-data">
-    
-    <div class="file-field input-field">
-      <div class="btn">
-        <span>File</span>
-        <input type="file" label = "Choose a music file">
-      </div>
-      
-      <div class="file-path-wrapper">
-        <input class="file-path validate" type="text">
-      </div>
+    <div class="col s4">
+        <div class="card  red darken-4 ">
+            <div class="card-content white-text">
+              <span class="card-title">Upload Music !</span>
+              <p>Wanna hear new songs ? Just upload one by selecting a song or even an album !</p>
+            </div>
+            
+        </div>
+    </div>
+    <div class="col s8">
+        
+    <form method="post" enctype="multipart/form-data">
+        
+        <div class="row">
+        <div class="file-field input-field">
+            <div class="btn  red darken-4">
+                <span>Select Music</span>
+                <input type="file" >
+            </div>
+            <div class="file-path-wrapper">
+                <input class="file-path validate" type="text" placeholder="Upload one or more files">
+            </div>
+        </div>
+            
+        </div>
+        
+        <input type="submit" class="waves-effect waves-light btn right  red darken-4">
+
+    </form>
     </div>
     
-    <input type="submit" class="waves-effect waves-light btn right">
     
-    
-      
-
-</form>
-</div>
 </div>
 
 
